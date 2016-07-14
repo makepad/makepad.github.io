aa85675fca7528a332e21a800a91cd8cb6c80a80
MakepadDev <info@makepad.io>
2016-07-14 16:33:23 +0200

    working rect

--- tests/canvastest.js
+++ tests/canvastest.js
@@ -4,19 +4,21 @@ var App = require('view').extend(function(){
 	this.nested = {
 		Background: require('shaders/rectshader').extend(function(){
 			this.pixel = function(){
-				return mix('red','green',mesh.y)
+				return props.color
 			}
 		})
 	}
 
 	this.ondraw = function(){
-		this.drawBackground({
-			color:'orange',
-			x:-.5,
-			y:-.5,
-			w:1,
-			h:1
-		})
+		for(var i = 0 ;i < 10000; i++){
+			this.drawBackground({
+				color:[0,.5+.5*Math.sin(i*0.1),.5+.5*Math.sin(i*0.1),1],
+				x:-.5 + Math.sin(i*.01)*.5+.5,
+				y:-.5 + Math.cos(i*.03)*.5+.5,
+				w:.05,
+				h:.05
+			})
+		}
 	}
 })
 App().runApp()
