8ac0e9cb63b69a72611f61fe6d9c4d33ce2254a6
MakepadDev <info@makepad.io>
2016-07-15 19:28:19 +0200

    round

--- tests/canvastest.js
+++ tests/canvastest.js
@@ -8,7 +8,7 @@ var App = require('view').extend(function(){
 
 	this.ondraw = function(){
 		this.drawBackground({
-			color:'purple',
+			color:'white',
 			borderradius:[20,10,20,10], // LT RT RB LB
 			shadowspread:-5,
 			shadowblur:10,
