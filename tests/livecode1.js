42543de7f808dc92fcf8bfc50c83ca5149f6f0ca
MakepadDev <info@makepad.io>
2016-08-18 20:53:30 +0200

    shader!

--- tests/livecode1.js
+++ tests/livecode1.js
@@ -1,17 +1,22 @@
 module.exports=require('apps/drawapp').extend({
 tools:{Rect:{
 col:'red',
+id:0,
 pixel:function(){$
-return mix('#7e2',this.col,abs(
+return mix('red',this.col,abs(
 sin(8.*length(
-this.mesh.xy-vec2(.5)
+this.mesh.xy-vec2(.5+.5*sin(this.time+this.id))
 )+this.time)
 ))
 }
 }},
+onFingerDown:function(){
+this.redraw()
+},
 onDraw:function(){
 for(var i=0;i<100;i++)
 this.drawRect({
+id:i,
 col:[random(),random(),random(),1],
 x:(i%10)*100,
 y:floor(i/10)*100,
