306f4e36ea6f6ca0ff5ce7aa452f419843415197
MakepadDev <info@makepad.io>
2016-12-06 20:59:19 +0100

    nearly there

--- libs/stamps/button.js
+++ libs/stamps/button.js
@@ -9,7 +9,7 @@ module.exports = class Button extends require('base/stamp'){
 			id:'',
 			index: 0,
 			onClick: undefined,
-			margin:[5,0,5,0],
+			margin:[2,0,2,0],
 			onClickStamp: undefined,
 			debug: 0
 		}
