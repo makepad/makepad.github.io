9c42eee35d2fb3c00f345e2c62bc99990f9e7037
MakepadDev <info@makepad.io>
2016-12-07 14:14:15 +0100

    errormsg

---
+++ examples/gameshow.js
@@ -0,0 +1,42 @@
+new require('styles/dark')
+module.exports = class extends require('base/drawapp'){ //top
+	prototype() {
+		this.tools = {
+			Text:{},
+			Quad:{
+				pixel:function() {$
+					
+					this.viewport()
+					this.circle(50., 50., 20.)
+					this.circle(50. + 20, 80., 30.)
+					this.gloop(30.)
+					this.fillKeep('orange')
+					this.stroke('blue', 4.)
+					
+					//this.rectangle(0.,0.,100.,100.)
+					//this.fill('blue')
+					
+					//this.circle(70.,60.,30.)
+					//this.fill('#f0fc')
+					
+					//this.box(50.,50.,50.,50.,10.)
+					//this.fill('#f')
+					//this.rotate(this.time,50,75)
+					//this.moveTo(100,100)
+					//this.lineTo(50,50)
+					//this.lineTo(50,150)
+					//this.closePath()
+					//this.strokeKeep('#f00',5.)
+					//this.blur=18.
+					//this.glow('#f00',15.)
+					
+					return this.result
+				}
+			}
+		}
+	}
+	
+	onDraw() {
+		this.drawQuad({w:'100%', h:'100%'})
+	}
+}
