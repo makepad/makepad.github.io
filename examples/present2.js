50840fc5a5136acbda7c9bebffb865108ed04ec2
MakepadDev <info@makepad.io>
2017-05-03 17:15:08 +0200

    push

---
+++ examples/present2.js
@@ -0,0 +1,135 @@
+new require('styles/dark')
+
+module.exports = class extends require('base/drawapp'){ //top
+	prototype() {
+		
+		this.xOverflow = 'none'
+		this.yOverflow = 'none'
+		this.props = {
+			page:_=module.worker.page || 0
+		}
+		this.tools = {
+			Splash:require('base/view').extend({
+				props :{text:'HI'},
+				tools :{
+					Bg  :require('shaders/quad').extend({
+						padding  :130,
+						fillColor:'orange',
+						//dump     :1,
+						pixel    :function() {$
+							this.viewport(this.mesh.xy)
+							this.translate(.5, .5)
+							this.circle(0., 0., .35) //+sin(this.time*8))
+							let p = this.pos
+							this.shape += 0.05 * abs(sin(atan(p.y, p.x) * 8 + this.time * 1))
+							this.fillKeep(this.fillColor)
+							this.strokeKeep('#44ffff', .02)
+							this.shape += 0.08
+							this.strokeKeep('blue', .03)
+							this.blur = 0.1
+							this.glow('white', 0.1)
+							return this.result
+						}
+					}),
+					Text:require('shaders/text').extend({
+						font        :require('fonts/ubuntu_monospace_256.font'),
+						fontSize    :62,
+						align       :[0.5, 0.5],
+						boldness    :0.,
+						outlineWidth:0.04,
+						color       :'#ffffffff',
+						shadowColor :'#00000099',
+						shadowBlur  :1.0,
+						shadowSpread:0.,
+						shadowOffset:[2., 2],
+						dy          :-4.1,
+						lineSpacing :0.9,
+						outlineColor:'#000000ff',
+						vertexStyle :function() {$
+							let b = this.bouncy = abs(sin(this.time))
+							this.shadowOffset = vec2(b * 10, b * 10)
+						},
+						vertexPos   :function(pos) {$
+							//return pos
+							this.pos = pos
+							let cen = vec2(this.viewSpace.x * .5, this.viewSpace.y * .53)
+							this.scale((this.bouncy * .8 + 1.5), cen.x, cen.y)
+							this.rotate(this.bouncy * .25, cen.x, cen.y)
+							return this.pos
+						},
+					}),
+				},
+				onDraw:function() {
+					this.beginBg({fillColor:this.color})
+					this.drawText({text:this.text})
+					this.endBg()
+				}
+			}),
+			
+		}
+	}
+	
+	constructor() {
+		super()
+		/*
+		Makepad
+		
+		*/
+		this.pages = [
+			{h:"What is a computer", q:"- A very fast fileclerk\n"},
+			{h:"What computes?", q:"- a Central Processing Unit (CPU)\n- a Graphics Processing Unit (GPU)"},
+			{h:"How do we program them?", q:"- Programming languages\n - JavaScript\n - Java\n - Python\n - Ruby"},
+			{h:"What is the web", q:"- Network of computers with\n- Servers (databases, etc)\n- Clients (webbrowser)"},
+			{h:"What is JS", q:"- Programming language that runs in the browser"}
+		]
+	}
+	
+	onKeyDown(e) {
+		if(e.name === 'leftArrow') {
+			this.page = max(0, this.page - 1)
+		}
+		if(e.name === 'rightArrow') {
+			this.page = min(this.pages.length, this.page + 1)
+		}
+		
+		module.worker.page = this.page
+		this.redraw()
+	}
+	
+	onDraw() {
+		this.beginBg({
+			color:'#1e191eff',
+			w    :'100%',
+			h    :'100%'
+		})
+		var scale = 0.6
+		var panel = 300
+		_=this.page
+		if(this.page == 0) {
+			this.drawSplash({id:0, text:'JavaScript'})
+		}
+		else {
+			
+			this.drawText({
+				fontSize:50 * scale,
+				margin  :[0, 0, 0, 40],
+				align   :[0., 0],
+				text    :this.pages[this.page - 1].h
+			})
+			this.lineBreak()
+			this.turtle.wy += 40
+			this.drawText({
+				fontSize:32 * scale,
+				margin  :[0, 0, 0, 40],
+				text    :this.pages[this.page - 1].q
+			})
+			this.turtle.wy += 20
+			this.drawText({
+				fontSize:20 * scale,
+				margin  :[0, 0, 0, 40],
+				text    :this.pages[this.page - 1].c
+			})
+		}
+		this.endBg()
+	}
+}
---
+++ examples/shop2.js
@@ -0,0 +1,215 @@
+new require('styles/dark')
+var painter = require('services/painter')
+module.exports = class extends require('base/app'){ //top
+	prototype() {
+		this.tools = {
+			Bg     :require("shaders/bg").extend({
+				borderRadius:5,
+				color       :'#ffffffff'
+			}),
+			HeatMap:require("shaders/image").extend({
+				pickAlpha   :1.,
+				imageSampler:{kind:'sampler', sampler:painter.SAMPLER2DLINEAR},
+				pixel       :function() {
+					var col = texture2D(this.imageSampler, this.mesh.xy)
+					var f = clamp(col.x * 10., 0., 1.)
+					return mix('#0000', mix('#00ff0fd4', '#ff060082', f), f)
+				}
+			}),
+			Image  :require("shaders/image").extend({
+			}),
+			Text   :require('shaders/text').extend({
+				color   :'#3d3d3dff',
+				wrapping:'word',
+				fontSize:24,
+				font    :require('fonts/ubuntu_regular_256.font')
+			}),
+			Icon   :require('shaders/text').extend({
+				color   :'#3d3d3dff',
+				fontSize:24,
+				font    :require('fonts/fontawesome_makepad.font')
+			}),
+			Line   :require('shaders/line').extend({
+				color:'black'
+			}),
+			Button :require('views/button').extend({
+			})
+		}
+	}
+	
+	constructor() {
+		super()
+		this.products = [
+			{name:'Mahogany obsidian', price:70, image:require('./images/shop1.jpg')},
+			{name:'Imperial jasper green', price:60, image:require('./images/shop2.jpg')},
+			{name:'Lake superior agate', price:70, image:require('./images/shop3.jpg')},
+			{name:'Binghamite', price:70, image:require('./images/shop4.jpg')},
+			{name:'Imperial jasper red', price:50, image:require('./images/shop5.jpg')},
+			{name:'Titanium ring cube', price:150, image:require('./images/shop6.jpg')},
+		]
+		
+		this.cart = module.worker.cart || (module.worker.cart = [])
+		this.heatmap = this.createTexture({
+			w:16,
+			h:16
+		})
+	}
+	
+	onFingerHoverGlobal(e) {
+		var l = this.toLocal(e)
+		var mx = (l.x / this.xmap) * this.heatmap.w
+		var my = (l.y / this.ymap) * this.heatmap.h
+		var idx = parseInt(mx) + parseInt(my) * this.heatmap.w
+		this.heatmap.array[idx]++
+		this.heatmap.update()
+	}
+	
+	onDraw() {
+		this.beginBg({
+			w         :'100%',
+			h         :'100%',
+			moveScroll:0
+		})
+		
+		this.beginBg({
+			w      :'100%',
+			margin :[2, 0, 0, 0],
+			padding:5,
+			color  :'#d7d7d7ff'
+		})
+		
+		this.Bg2 = _=>{
+			w:'100%'
+			margin:[2, 0, 0, 0]
+			padding:5
+			color:'#d7d7d7ff'
+		}
+		
+		this.drawText({
+			align:[0.5, 0],
+			color:'#949494ff',
+			text :'Web Shop'
+		})
+		this.endBg()
+		for(var i = 0;i < this.products.length;i++){
+			let product = this.products[i]
+			this.beginBg({
+				margin :[2, 0, 0, 0],
+				padding:5,
+				color  :'#d7d7d7ff'
+			})
+			this.drawImage({
+				image:product.image,
+				w    :100,
+				h    :100
+			})
+			this.beginBg({
+				margin :[0, 0, 0, 5],
+				padding:[0, 10, 0, 10],
+				w      :150,
+				color  :'#c8c3c3ff'
+			})
+			this.drawText({
+				//margin:[40, 10, 0, 10],
+				fontSize:20,
+				text    :product.name,
+			})
+			this.lineBreak()
+			this.drawText({
+				fontSize:14,
+				italic  :-0.1,
+				text    :'Price: ' + product.price + ' Euros'
+			})
+			this.endBg()
+			this.drawButton({
+				icon   :'shopping-basket',
+				id     :'btn' + i,
+				item   :i,
+				onClick:btn=>{
+					this.cart.push(product)
+					this.redraw()
+				}
+			})
+			this.endBg()
+			this.lineBreak()
+		}
+		this.beginBg({
+			margin :[2, 0, 0, 0],
+			y      :45,
+			w      :100,
+			align  :[0.97, 0],
+			padding:5,
+			color  :'#d7d7d7ff'
+		})
+		this.drawText({
+			color   :'#949494ff',
+			fontSize:20,
+			text    :'Cart'
+		})
+		var total = 0
+		for(var i = 0;i < this.cart.length;i++){
+			total += this.cart[i].price
+		}
+		this.lineBreak()
+		this.drawText({
+			color   :'#949494ff',
+			fontSize:10,
+			text    :'Total:' + total + ' Euros'
+		})
+		this.lineBreak()
+		
+		for(var i = 0;i < this.cart.length;i++){
+			let product = this.cart[i]
+			this.beginBg({
+				color  :'#a9a9a9ff',
+				w      :90,
+				padding:8
+			})
+			
+			this.drawImage({
+				image :product.image,
+				margin:[0, 0, 5, 0],
+				w     :50,
+				h     :50
+			})
+			this.lineBreak()
+			this.drawText({
+				color   :'#000000ff',
+				fontSize:10,
+				wrapping:'word',
+				text    :product.name
+			})
+			this.drawText({
+				color   :'#000000ff',
+				fontSize:10,
+				wrapping:'word',
+				text    :'\nEuro: ' + product.price
+			})
+			this.lineBreak()
+			this.drawButton({
+				id     :'rem' + i,
+				text   :'Remove',
+				icon   :'close',
+				onClick:btn=>{
+					var i = this.cart.indexOf(product)
+					this.cart.splice(i, 1)
+					this.redraw()
+				}
+			})
+			this.endBg({})
+		}
+		this.endBg()
+		
+		this.endBg(true)
+		this.xmap = this.turtle.x2
+		this.ymap = this.turtle.y2
+		this.drawHeatMap({
+			order:2,
+			x    :0,
+			y    :0,
+			w    :this.xmap,
+			h    :this.ymap,
+			image:this.heatmap
+		})
+	}
+}
